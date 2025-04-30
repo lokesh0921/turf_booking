b8825e51af5f64383c26f33e282707b1f2672eb5
jaybutle <itzjaybutle@Jays-macbook.local>
2025-04-30 13:25:50 +0530

    Frontend updated by jay on 30 aril 1 pm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import React from "react";
+import LoginPage from "../pages/login"; // adjust path if it's in a different folder
 
-function App() {
-  return (
-    <>
-      <h1>hello devs from lokesh</h1>
-    </>
+function App(){
+  return(
+    <div><LoginPage/></div>
   );
 }
+
+//function App() {
+  //return (
+    //<>
+      //<h1>hello devs from lokesh</h1>
+    //</>
+//  );
+//}
 
 export default App;
