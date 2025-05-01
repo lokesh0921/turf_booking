d43750dd222ea9f9c5dfc4b1895527930496149a
Lokesh Pawar <lokeshpawar921@gmail.com>
2025-05-01 17:21:18 +0530

    all wordking code with css files added lokesh 1may 5.21

--- frontend/src/pages/Signuppage.jsx
+++ frontend/src/pages/Signuppage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+// import styles from "./signup.module.css";
+import styles from "./signup.module.css";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return <div className={styles.divexample}>SignupPage</div>;
 };
 
 export default SignupPage;
